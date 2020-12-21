9b3c57ce285912bcc55997f9faa83e8ebb39037b
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-21 15:18:18 -0800

    forming ajax request for city input

---
+++ assets/js_files/script.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    var apiKey = b49c26cbda0627b6ceb0dc8596060a98;
+    var city = "San Diego";
+
+
+    // creating for form submission!
+    // $(document).on("click", ".city", function () {
+    $(document).on("submit", function () {
+
+        // var city = $(this).attr("data-city");
+
+        var queryUrl =
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=imperial&appid=" +
+            apiKey;
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET",
+        }).then(function (data) {
+
+            console.log(data);
+
+            $(".city_name").text(data.name + " Weather")
+            $("#temp").text(data.main.temp +  " °")
+            $("#wind").text(data.wind.speed +  " MPH")
+            $("#humidity").text(data.main.speed +  " %")
+
+
+
+        })
+
+
+
+
+
+        /* Make code that works for the buttons!!! */
+        // $(document).on("click", ".city", function () {
+
+
+    }
