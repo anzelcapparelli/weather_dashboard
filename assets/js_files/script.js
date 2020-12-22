f1eaeb7ea1b6ee7c2ac8c27ae091d4c60053f9ef
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-21 18:53:52 -0800

    linked js file to html file; modifications to ajax request

--- assets/js_files/script.js
+++ assets/js_files/script.js
@@ -1,36 +1,53 @@
 $(function () {
 
-    var apiKey = b49c26cbda0627b6ceb0dc8596060a98;
+    var apiKey = "b49c26cbda0627b6ceb0dc8596060a98";
     var city = "San Diego";
 
+    var queryUrl =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        apiKey;
 
-    // creating for form submission!
-    // $(document).on("click", ".city", function () {
-    $(document).on("submit", function () {
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+    }).then(function (data) {
 
-        // var city = $(this).attr("data-city");
+        console.log(data);
 
-        var queryUrl =
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-            city +
-            "&units=imperial&appid=" +
-            apiKey;
-
-        $.ajax({
-            url: queryUrl,
-            method: "GET",
-        }).then(function (data) {
-
-            console.log(data);
-
-            $(".city_name").text(data.name + " Weather")
-            $("#temp").text(data.main.temp +  " °")
-            $("#wind").text(data.wind.speed +  " MPH")
-            $("#humidity").text(data.main.speed +  " %")
+    })
 
 
+        // // creating for form submission!
+        // // $(document).on("click", ".city", function () {
+        // $(document).on("submit", function (event) {
 
-        })
+        // event.preventDefault;
+
+        // // var city = $(this).attr("data-city");
+
+        // var queryUrl =
+        //     "https://api.openweathermap.org/data/2.5/weather?q=" +
+        //     city +
+        //     "&units=imperial&appid=" +
+        //     apiKey;
+
+        // $.ajax({
+        //     url: queryUrl,
+        //     method: "GET",
+        // }).then(function (data) {
+
+        //     console.log(data);
+
+        //     $(".city_name").text(data.name + " Weather")
+        //     $("#temp").text(data.main.temp + " °")
+        //     $("#wind").text(data.wind.speed + " MPH")
+        //     $("#humidity").text(data.main.speed + " %")
+
+
+
+        // })
 
 
 
@@ -40,4 +57,4 @@ $(function () {
         // $(document).on("click", ".city", function () {
 
 
-    }
+    })
